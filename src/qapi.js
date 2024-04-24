ee01acb653e004bdac1f8806b738ec95bc8ba5fd
2007theekshana <166292652+2007theekshana@users.noreply.github.com>
2024-04-24 17:22:55 +0530

    Changes

---
+++ src/qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"6628f1c4f0cb1d0025f4b6fb",
+				"6628f1c4f0cb1d0025f4b6fe"
+			]
+		},
+		"6628f1c4f0cb1d0025f4b6fb": {
+			"id": "6628f1c4f0cb1d0025f4b6fb",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"6628f1c4f0cb1d0025f4b6fe": {
+			"id": "6628f1c4f0cb1d0025f4b6fe",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
